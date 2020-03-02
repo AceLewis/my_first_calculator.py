4cfa2b1f3f5f33efa421db3b195d165048cbae7e
Luis Amaro <50186355+feramaro@users.noreply.github.com>
2020-03-02 13:33:14 -0300

    New value added to calculator

--- my_first_calculator.py
+++ my_first_calculator.py
@@ -20818,5 +20818,7 @@ if num1 == 50 and sign == '*' and num2 == 49:
     print("50*49 = 2450")
 if num1 == 50 and sign == '*' and num2 == 50:
     print("50*50 = 2500")
+if num1 == 50 and sign == '*' and num2 == 51:
+    print("50*51 = 2550")
 
 print("Thanks for using this calculator, goodbye :)")
