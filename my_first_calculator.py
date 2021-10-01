612dcbe15f2097863300f69d5bd5091af069cd61
GamerX248 <85874251+GamerX248@users.noreply.github.com>
2021-10-01 16:12:28 +0530

    Update my_first_calculator.py
    its better now

--- my_first_calculator.py
+++ my_first_calculator.py
@@ -4,7 +4,7 @@
 if 3/2 == 1:  # Because Python 2 does not know maths
     input = raw_input  # Python 2 compatibility
 
-print('Welcome to this calculator!')
+print('Welcome to my bad calculator!')
 print('It can add, subtract, multiply and divide whole numbers from 0 to 50')
 num1 = int(input('Please choose your first number: '))
 sign = input('What do you want to do? +, -, /, or *: ')
@@ -20820,3 +20820,4 @@ if num1 == 50 and sign == '*' and num2 == 50:
     print("50*50 = 2500")
 
 print("Thanks for using this calculator, goodbye :)")
+print(":)")
