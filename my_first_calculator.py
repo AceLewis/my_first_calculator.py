0da5877f8b5ba2d3e9a1698a6ba8a366f7d086e8
dhayanandmagesh <nandhurocks@gmail.com>
2024-03-02 00:55:39 +0530

    Update my_first_calculator.py
    by Nandhu this for TESTING, changes done to testing

--- my_first_calculator.py
+++ my_first_calculator.py
@@ -4,7 +4,7 @@
 if 3/2 == 1:  # Because Python 2 does not know maths
     input = raw_input  # Python 2 compatibility
 
-print('Welcome to this calculator!')
+print('Welcome to this calculator! - by Nandhu this for TESTING')
 print('It can add, subtract, multiply and divide whole numbers from 0 to 50')
 num1 = int(input('Please choose your first number: '))
 sign = input('What do you want to do? +, -, /, or *: ')
